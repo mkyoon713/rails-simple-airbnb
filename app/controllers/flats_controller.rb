70a34b77891e5691385bec486b90195e1af042d8
Minkyeong Yoon <mkyoon713@gmail.com>
2018-11-21 17:44:45 +0900

    finished creating airbnb app

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,44 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flats_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flats_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flats_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name: 'Ice Castle in Sweden',
+    address: '11 Ice Garden Stockholm 1392',
+    description: 'A lovely but cold ice house',
+    price_per_night: 100,
+    number_of_guests: 8
+  },
+  {
+    name: 'Garden Flat in Seoul',
+    address: '15 Jongro 1 Ga 1243 Seoul 1743',
+    description: 'Flat in middle of Seoul with rooftop garden',
+    price_per_night: 55,
+    number_of_guests: 2
+  },
+  {
+    name: 'Boat House in Boston',
+    address: '11 Dempsey Street Boston 13432',
+    description: 'A waterfrot boathouse with amazing free lobsters',
+    price_per_night: 60,
+    number_of_guests: 4
+  },
+]
+Flat.create!(flats_attributes)
+puts 'Finished!'
+
